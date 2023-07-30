34c3ad09e02e05d309f35f53f6a921b391c7ee42
Charly Brown <2131406-carlosmonti@users.noreply.gitlab.com>
2023-07-30 03:40:42 -0300

    feat: adding eslint and prettier config

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -7,6 +7,7 @@ module.exports = {
     "plugin:react-hooks/recommended",
     "plugin:react/recommended",
     "plugin:react/jsx-runtime",
+    "plugin:prettier/recommended",
   ],
   ignorePatterns: ["dist", ".eslintrc.cjs"],
   parser: "@typescript-eslint/parser",
@@ -16,11 +17,33 @@ module.exports = {
     project: ["./tsconfig.json", "./tsconfig.node.json"],
     tsconfigRootDir: __dirname,
   },
-  plugins: ["react-refresh"],
+  plugins: ["react-refresh", "unused-imports", "simple-import-sort", "prettier"],
   rules: {
-    "react-refresh/only-export-components": [
+    "react-refresh/only-export-components": ["warn", { allowConstantExport: true }],
+    "no-unused-vars": "off", // or "@typescript-eslint/no-unused-vars": "off",
+    "unused-imports/no-unused-imports": "error",
+    "unused-imports/no-unused-vars": [
       "warn",
-      { allowConstantExport: true },
+      {
+        vars: "all",
+        varsIgnorePattern: "^_",
+        args: "after-used",
+        argsIgnorePattern: "^_",
+      },
     ],
+    "simple-import-sort/imports": "error",
+    "sort-vars": "error",
+    "sort-keys": "error",
+    "prettier/prettier": ["error"],
+    "react/jsx-sort-props": ["error", {
+      "ignoreCase": true,
+      "callbacksLast": true,
+      "shorthandFirst": true,
+      "multiline": 'last',
+      "reservedFirst": true
+    }],
+    "operator-linebreak": ["error", "before"],
+    "arrow-body-style": "off",
+    "prefer-arrow-callback": "off"
   },
 };
